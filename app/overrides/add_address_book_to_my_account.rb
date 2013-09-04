b53e2791d9e3d142efa317b662cabded6e6129e6
Jaime <jaime@synaptian.com>
2013-09-04 14:35:55 -0400

    Fixing formatting in overrides.

--- app/overrides/add_address_book_to_my_account.rb
+++ app/overrides/add_address_book_to_my_account.rb
@@ -3,5 +3,6 @@ Deface::Override.new(
   :name => "address_book_account_my_orders",
   :insert_after => "[data-hook='account_my_orders'], #account_my_orders[data-hook]",
   :partial => "spree/users/addresses",
-  :disabled => false
+  :disabled => false,
+  :original => 'f1f0e9b7901295ea2f4dedaa53efd632aaa2d26e'
 )
--- app/overrides/add_shipping_address_link_to_button.rb
+++ app/overrides/add_shipping_address_link_to_button.rb
@@ -1,6 +1,7 @@
-Deface::Override.new(:virtual_path  => "spree/users/show",
-                     :replace => "div#new_address_link",
-                     :text          => "<div id='new_address_link'>
-                                <br /> <a href='/addresses/new'><button>Add new address</button></a>
-                                        </div>",
-                     :name          => "address_link_button")
+Deface::Override.new(
+  :virtual_path => "spree/users/show",
+  :name          => "address_link_button",
+  :replace => "div#new_address_link",
+  :disabled => false,
+  :text          => "<div id='new_address_link'><br /><a href='/addresses/new'><button>Add new address</button></a></div>"
+)
