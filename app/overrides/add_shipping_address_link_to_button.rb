3c0e475c495345e1f4664d6bcef2c7206d30fc92
Jaime <jaime@synaptian.com>
2013-09-04 14:47:03 -0400

    Properly adding button styling to adding addresses.

--- app/overrides/add_shipping_address_link_to_button.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  :virtual_path => "spree/users/show",
-  :name          => "address_link_button",
-  :replace => "div#new_address_link",
-  :disabled => false,
-  :text          => "<div id='new_address_link'><br /><a href='/addresses/new'><button>Add new address</button></a></div>"
-)
